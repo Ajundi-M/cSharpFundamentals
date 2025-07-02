e46ab42f11aa2c6971bc2cb3a8974c02dabaa977
Moustapha Al.Jundi <ms.jundi@live.com>
2025-07-02 19:34:21 +0200

    Refactor Program.cs for variable setup and greeting
    Removed user input prompts and added variable declarations for
    monthly wage, bonus, and other metrics. Updated greeting
    message construction from string interpolation to concatenation.

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,3 +1,21 @@
-﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
-Console.WriteLine("Please enter your name:");
-Console.ReadLine();
+﻿//string name_2 = Console.ReadLine();
+//string Name_2 = Console.ReadLine();
+///string 2Name = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+//byte numberOfEmployees = 300;
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+hoursWorked = 148;
+
+//monthlyWage = true;
--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -4,4 +4,4 @@ Console.Write("Please enter your first name: ");
 String firstName = Console.ReadLine();
 Console.Write("Please enter your last name: ");
 String lastName = Console.ReadLine();
-Console.WriteLine($"hello, " + firstName + " " + lastName);
+Console.WriteLine("hello, " + firstName + " " + lastName);
